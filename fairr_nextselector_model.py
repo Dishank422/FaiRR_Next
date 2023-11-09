3ee0bdda0d51c6ef4413126dd963c7f1663c768a
Dishank422 <dishankjain0112358@gmail.com>
2023-11-09 23:41:34 +0530

    added configs for nextinference

--- fairr_nextselector_model.py
+++ fairr_nextselector_model.py
@@ -225,7 +225,7 @@ class FaiRRNextSelector(BaseModel):
             for metric in perf_metrics.keys():
                 self.log(f'facts {split}_{metric}', perf_metrics[metric], on_step=True, on_epoch=True)
 
-        lambd = 10
+        lambd = 50
         loss = rule_loss+lambd*fact_loss
         self.log('loss', loss.item(), prog_bar=True, on_step=True, on_epoch=True)
         return {'loss': loss}
